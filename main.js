8e13effdced4df7a3be7744c403adf3ee0dc0b5a
Agustin Coria <soyagustincoria@gmail.com>
2024-05-07 20:21:19 -0300

    transition nav

--- main.js
+++ main.js
@@ -2,6 +2,6 @@
 const navList = document.querySelector(".nav-list");
 const navToggle = document.querySelector(".nav-toggle");
 navToggle.addEventListener("click", () => {
-    navList.classList.toggle("hidden-mobile");
+    navList.classList.toggle("nav-open");
     navToggle.classList.toggle("nav-open");
 });
