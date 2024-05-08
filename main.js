cece07c89c918a70fb85383e0623a22213fbc23a
Agustin Coria <soyagustincoria@gmail.com>
2024-05-07 21:17:20 -0300

    nav toggle icon

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 "use strict"
 const navList = document.querySelector(".nav-list");
 const navToggle = document.querySelector(".nav-toggle");
+let toggleIcon = document.querySelector(".material-symbols-outlined");
 navToggle.addEventListener("click", () => {
     navList.classList.toggle("nav-open");
-    navToggle.classList.toggle("nav-open");
+    let iconOpen = toggleIcon.getAttribute("data-open");
+    
+    if (iconOpen=="false") {
+        toggleIcon.innerHTML="close";
+        toggleIcon.setAttribute('data-open', true);
+    } else {
+        toggleIcon.innerHTML="menu";
+        toggleIcon.setAttribute('data-open',false);
+    }
 });
