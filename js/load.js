2eeaa2cb1a693b703f4eda75c548597e88ac80ef
Agustin Coria <soyagustincoria@gmail.com>
2024-06-17 20:27:52 -0300

    agregado partial render

---
+++ js/load.js
@@ -0,0 +1,46 @@
+"use strict"
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector("main");
+
+    function selectTab(id) {
+        document.querySelectorAll(".nav-element").forEach((item) => item.classList.remove("selected"));
+        document.querySelectorAll("#"+id).forEach((item) => item.classList.add("selected"));
+    }
+
+    async function loadContent(id) {
+        const container = document.querySelector("#load");
+        container.innerHTML = "Cargando "+id+"...";
+        let url = id+".html";
+        try {
+            let response = await fetch(url);
+            if(response.ok){
+                let t = await response.text();
+                container.innerHTML = t;
+            }else{
+                container.innerHTML = "Página no encontrada";
+            }
+        } catch (error) {
+            container.innerHTML = "Error de conexión";
+        }
+    }
+
+    function push(event) {
+        let id = event.target.id;
+        selectTab(id);
+        document.title=id;
+        loadContent(id);
+        window.history.pushState({id}, id, '/'+id);
+    }
+
+    window.onload = (event) =>{
+        window["home"].addEventListener("click", (event) => push(event));
+        window["about"].addEventListener("click", (event) => push(event));
+        window["contact"].addEventListener("click", (event) => push(event));
+    };
+
+    window.addEventListener("popstate", (event) => {
+        let stateId = event.state.id;
+        selectTab(stateId);
+        loadContent(stateId);
+    });
+});
