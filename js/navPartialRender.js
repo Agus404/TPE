b6931903e34965b1511070f083c669ea3b99eb58
Agustin Coria <soyagustincoria@gmail.com>
2024-06-20 17:18:09 -0300

    solucionados bugs en contact

--- js/navPartialRender.js
+++ js/navPartialRender.js
@@ -6,43 +6,50 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelectorAll(`#${id}`).forEach((item) => item.classList.add("selected"));
     }
 
-    function cargarCaptcha() {
-        const btnRecargar = document.querySelector("#btn-recargar");
+    
+    function generateCaptcha() {
         let feedback = document.querySelector(".feedback");
-        btnRecargar.addEventListener("click", generateCaptcha);
-        generateCaptcha();
-        function generateCaptcha() {
-            document.querySelector("#captcha").value = "";
-            const randomChar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-            let codigoCaptcha = "";
-            for (let i = 0; i < 5; i++) {
-                codigoCaptcha += randomChar[Math.floor(Math.random() * randomChar.length)];
+        document.querySelector("#captcha").value = "";
+        const randomChar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        let codigoCaptcha = "";
+        for (let i = 0; i < 5; i++) {
+            codigoCaptcha += randomChar[Math.floor(Math.random() * randomChar.length)];
+        }
+        document.querySelector(".preview").innerHTML = codigoCaptcha;
+
+        const btnComprobar = document.querySelector("#btn-comprobar");
+        btnComprobar.addEventListener("click", () => {
+            const btnSubmit = document.querySelector("#btn-submit");
+            const btnRecargar = document.querySelector("#btn-recargar");
+            let captcha = document.querySelector("#captcha").value;
+            if (captcha == codigoCaptcha) {
+                feedback.innerHTML = "Captcha correcto";
+                feedback.style.color = 'green';
+                btnSubmit.disabled = false;
+                btnComprobar.disabled = true;
+                btnRecargar.disabled = true;
+            } else {
+                feedback.innerHTML = "Captcha incorrecto";
+                feedback.style.color = 'red';
             }
-            document.querySelector(".preview").innerHTML = codigoCaptcha;
-
-            const btnComprobar = document.querySelector("#btn-comprobar");
-            btnComprobar.addEventListener("click", () => {
-                const btnSubmit = document.querySelector("#btn-submit");
-                let captcha = document.querySelector("#captcha").value;
-                if (captcha == codigoCaptcha) {
-                    feedback.innerHTML = "Captcha correcto";
-                    feedback.style.color = 'green';
-                    btnSubmit.disabled = false;
-                    btnComprobar.disabled = true;
-                    btnRecargar.disabled = true;
-                } else {
-                    feedback.innerHTML = "Captcha incorrecto";
-                    feedback.style.color = 'red';
-                }
-            });
-        }
-
+        });
+    }
+    
+    function enviar(e) {
+        e.preventDefault();
         const form = document.querySelector("#form");
-        form.addEventListener("submit", enviar);
-        function enviar(e) {
-            e.preventDefault();
-            feedback.innerHTML = "Enviado";
-        }
+        let formData = new FormData(form);
+        let nombre = formData.get("nombre");
+        let email = formData.get("email");
+        let observaciones = formData.get("observaciones");
+        console.log(`Nombre: ${nombre}. Email: ${email}. Observaciones: ${observaciones}.`);
+        document.querySelector(".feedback").innerHTML = "Enviado";
+    }
+
+    function cargarCaptcha() {
+        document.querySelector("#btn-recargar").addEventListener("click", generateCaptcha);
+        generateCaptcha();
+        document.querySelector("#form").addEventListener("submit", (enviar));
     }
 
     async function loadContent(id) {
