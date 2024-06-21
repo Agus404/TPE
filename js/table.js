e15222922294ddaafd73bb94bf2c5e3feef815f5
Agustin Coria <soyagustincoria@gmail.com>
2024-06-21 02:34:14 -0300

    Agregado metodo POST

--- js/table.js
+++ js/table.js
@@ -18,10 +18,10 @@
                     let categoria = element.categoria;
                     let costo = element.costo;
                     let id = element.id;
-                    tabla.innerHTML += `<tr><td>${id}</td><td>${categoria}</td><td>${servicio}</td><td>${costo}</td><td><button type="button" class="btn-eliminar">X</button></td></tr>`
+                    tabla.innerHTML += `<tr><td>${id}</td><td>${categoria}</td><td>${servicio}</td><td>$${costo}</td><td><button type="button" class="btn-eliminar">X</button></td></tr>`
                     total+=element.costo;
                 }
-                tabla.innerHTML+=`<tr><td></td><td></td><td>TOTAL:  ${total}</td><td></td></tr>`
+                tabla.innerHTML+=`<tr><td></td><td></td><td>TOTAL:  $${total}</td><td></td><td></td></tr>`
             } else {
                 console.log("Página no encontrada");
             }
@@ -29,10 +29,91 @@
             console.log("Error de conexión");
         }
     }
-
+    
     async function agregarElemento() {
         let url = new URL('https://6671f0a2e083e62ee43d9e57.mockapi.io/tpe/services');
-        let listaServicios = document.querySelector("#service");
+        const objetosServicios = [{
+            "servicio": "Estrategia de marca",
+            "categoria": "Estrategia",
+            "costo": 300
+        },
+        {
+            "servicio": "Estrategia de producto",
+            "categoria": "Estrategia",
+            "costo": 200
+        },
+        {
+            "servicio": "Estrategias de contenido",
+            "categoria": "Estrategia",
+            "costo": 100
+        },
+        {
+            "servicio": "Investigacion y análisis",
+            "categoria": "Estrategia",
+            "costo": 200
+        },
+        {
+            "servicio": "Identidad de marca",
+            "categoria": "Branding",
+            "costo": 250
+        },
+        {
+            "servicio": "Narrativa de marca",
+            "categoria": "Branding",
+            "costo": 150
+        },
+        {
+            "servicio": "Dirección creativa",
+            "categoria": "Branding",
+            "costo": 150
+        },
+        {
+            "servicio": "Identidad audiovisual",
+            "categoria": "Branding",
+            "costo": 100
+        },
+        {
+            "servicio": "Marketing y campañas publicitarias",
+            "categoria": "Branding",
+            "costo": 150
+        },
+        {
+            "servicio": "Productos digitales",
+            "categoria": "Digital",
+            "costo": 350
+        },
+        {
+            "servicio": "Posicionamiento digital",
+            "categoria": "Digital",
+            "costo": 250
+        },
+        {
+            "servicio": "Diseño UI/UX",
+            "categoria": "Digital",
+            "costo": 200
+        },
+        {
+            "servicio": "Desarrollo web",
+            "categoria": "Digital",
+            "costo": 300
+        },
+        {
+            "servicio": "Desarrollo de aplicaciones",
+            "categoria": "Digital",
+            "costo": 350
+        }];
+        let listaServicio = document.querySelector("#service").value;
+        let i = 0;
+        while (objetosServicios[i].servicio!=listaServicio) {
+            i++;
+        }
+        
+        let objeto = {
+            "servicio": objetosServicios[i].servicio,
+            "categoria": objetosServicios[i].categoria,
+            "costo": objetosServicios[i].costo
+        }
+        
         // let objeto = {
         //     "servicio": ,
         //     "categoria": ,
@@ -49,7 +130,7 @@
             let res = await fetch(url, requestOptions);
          
             if (res.status == 201) {
-                cargarTabla();
+                obtenerTabla();
             }
          } catch (error) {
             console.log(error);
@@ -58,4 +139,6 @@
 
     function cargarTabla() {
         obtenerTabla();
+        const btnAgregar = document.querySelector("#btn-agregar");
+        btnAgregar.addEventListener("click", agregarElemento);
     }
