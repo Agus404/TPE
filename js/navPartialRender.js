2d195af4e67ea638502a814550b63dc2b5f3c51a
Agustin Coria <soyagustincoria@gmail.com>
2024-06-23 20:52:36 -0300

    terminado filter

--- js/navPartialRender.js
+++ js/navPartialRender.js
@@ -3,28 +3,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function selectTab(id) {
         document.querySelectorAll(".nav-element").forEach((item) => item.classList.remove("selected"));
-        document.querySelectorAll(`#${id}`).forEach((item) => item.classList.add("selected"));
+        document.querySelector(`#${id}`).classList.add("selected");
     }
 
     function cargarButtons() {
         const btnAbout = document.querySelector(".btn-about");
-        btnAbout.addEventListener("click", (event) =>push(event));
+        btnAbout.addEventListener("click", (event) =>push(event.target.getAttribute("data-href")));
 
         const btnContact = document.querySelector(".btn-contact");
-        btnContact.addEventListener("click", (event) =>push(event));
+        btnContact.addEventListener("click", (event) =>push(event.target.getAttribute("data-href")));
     }
 
     async function loadContent(id) {
-        // const container = document.querySelector("main");
         const container = document.querySelector("#load");
         container.innerHTML = `<h1 class="loading">Cargando ${id}...</h1>`;
-        // let url = id+".html";
         try {
             let response = await fetch(`${id}.html`);
             if (response.ok) {
                 let content = await response.text();
                 container.innerHTML = content;
-                if (id ==='home')
+                if (id ==="home")
                     cargarButtons();
                 if (id === 'contact')
                     cargarCaptcha();
@@ -38,25 +36,16 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // function push(event) {
-    //     let id = event.target.id;
-    //     selectTab(id);
-    //     loadContent(id);
-    //     window.history.pushState({ id }, id, `${id}`);
-    // }
-
-    function push(event) {
-        let id = event.target.getAttribute("data-href");
+    function push(id) {
         selectTab(id);
         loadContent(id);
         window.history.pushState({ id }, id, `${id}`);
     }
 
-    // window.onload = (event) =>{
-    window["home"].addEventListener("click", (event) => push(event));
-    window["about"].addEventListener("click", (event) => push(event));
-    window["contact"].addEventListener("click", (event) => push(event));
-    // };
+    
+    window["home"].addEventListener("click", (event) => push(event.target.getAttribute("data-href")));
+    window["about"].addEventListener("click", (event) => push(event.target.getAttribute("data-href")));
+    window["contact"].addEventListener("click", (event) => push(event.target.getAttribute("data-href")));
 
     window.addEventListener("popstate", (event) => {
         let stateId = event.state.id;
@@ -64,7 +53,5 @@ document.addEventListener('DOMContentLoaded', () => {
         loadContent(stateId);
     });
 
-    selectTab("home");
-    loadContent("home");
-    window.history.pushState("home", "home", "/home");
+    push('home');
 });
--- js/table.js
+++ js/table.js
@@ -1,5 +1,6 @@
 "use strict"
 const url = new URL('https://6671f0a2e083e62ee43d9e57.mockapi.io/tpe/services');
+// let page = 1;
 const objetosServicios = [{
     "servicio": "Estrategia de marca",
     "categoria": "Estrategia",
@@ -41,7 +42,7 @@ const objetosServicios = [{
     "costo": 100
 },
 {
-    "servicio": "Marketing y campañas publicitarias",
+    "servicio": "Campañas publicitarias",
     "categoria": "Branding",
     "costo": 150
 },
@@ -71,12 +72,15 @@ const objetosServicios = [{
     "costo": 350
 }];
 
-async function obtenerTabla() {
-    const tabla = document.querySelector("#tabla-presupuesto");
-    // tabla.innerHTML = `Cargando...`;
+async function obtenerTabla(urlTabla) {
+    // const urlParams = new URL(urlTabla);
+    // urlParams.searchParams.append('page', page);
+    // urlParams.searchParams.append('limit', 10);
+    
     try {
-        let res = await fetch(url);
+        let res = await fetch(urlTabla);
         if (res.ok) {
+            const tabla = document.querySelector("#tabla-presupuesto");
             tabla.innerHTML = ' ';
             let json = await res.json();
             let total = 0;
@@ -88,7 +92,6 @@ async function obtenerTabla() {
                 tabla.innerHTML += `<tr><td>${id}</td><td>${categoria}</td><td>${servicio}</td><td>$${costo}</td><td><button type="button" class="btn-eliminar btn-table" data-id="${id}">X</button><button type="button" class="btn-modificar btn-table" data-id="${id}">◄</button></td></tr>`
                 total += costo;
             }
-            // tabla.innerHTML += `<tr><td></td><td></td><td></td><td>TOTAL:  $${total}</td><td></td></tr>`;
             document.querySelector("#total").innerHTML=`TOTAL: $${total}`;
             let btnEliminar = document.querySelectorAll(".btn-eliminar");
             btnEliminar.forEach(btn => { btn.addEventListener("click", eliminarElemento); });
@@ -128,7 +131,7 @@ async function agregarElemento(objeto) {
     try {
         let res = await fetch(url, requestOptions);
         if (res.status == 201) {
-            obtenerTabla();
+            obtenerTabla(url);
         }
     } catch (error) {
         console.log(error);
@@ -136,48 +139,23 @@ async function agregarElemento(objeto) {
 }
 
 async function agregarCategoria(cat) {
-    // let categoria = objeto.categoria;
-    // alert(cat);
-    // document.querySelectorAll(`.${categoria}`).forEach((item) => agregarElemento(obtenerObjeto(objetosServicios)));
-
-    // for (const element of objetosServicios) {
-    //     alert(objetosServicios[element].servicio);
-    //     if (objetosServicios[element].categoria == cat) {
-    //         alert(objetosServicios[element].servicio);
-    //         agregarElemento(objetosServicios[element]);
-    //     }
-    // }
 
     for (let index = 0; index < objetosServicios.length; index++) {
         const element = objetosServicios[index];
         if (element.categoria==cat) {
             await agregarElemento(element);
-            // try {
-            //     let res = await fetch(url, {
-            //        'method': 'POST',
-            //         'headers': { 'Content-Type': 'application/json' },
-            //         'body': JSON.stringify(element) 
-            //     });
-            //     if (res.status == 201) {
-            //         console.log(`Agregado ${element.servicio}`);
-            //     }
-            // } catch (error) {
-            //     console.log(error);
-            // }
         }
     }
-    // obtenerTabla();
 }
 
 async function eliminarElemento(event) {
     let id = event.target.getAttribute("data-id");
-    alert(id);
     try {
         let res = await fetch(`${url}/${id}`, {
             'method': 'DELETE',
         });
         if (res.status === 200) {
-            obtenerTabla();
+            obtenerTabla(url);
         }
     } catch (error) {
         console.log("Error");
@@ -195,22 +173,45 @@ async function modificarElemento(event){
     try {
         let res = await fetch(`${url}/${id}`, requestOptions);
         if (res.status === 200){
-            obtenerTabla();
+            obtenerTabla(url);
         }
     } catch (error) {
         console.log("Error");
     }
 }
 
+function filter(event) {
+    let id = event.target.getAttribute("data-filter")
+    document.querySelectorAll(".tag").forEach((item) => item.classList.remove("selected"));
+    if (id==" ") {
+        document.querySelector("#reset-tags").classList.add("hidden");
+        obtenerTabla(url);
+    } else {
+        document.querySelector("#reset-tags").classList.remove("hidden");
+        event.target.classList.add("selected");
+        const params = new URL(url);
+        params.searchParams.append('categoria', id);
+        obtenerTabla(params);
+    }
+}
+
+// function actualizarPage(event) {
+//     const id = event.target.id;
+//     if (id=='prev' && page>1)
+//         page--;
+//     if (id=='next')
+//         page++;
+//     obtenerTabla(url);
+// }
+
 function cargarTabla() {
-    obtenerTabla();
+    obtenerTabla(url);
     const btnAgregar = document.querySelector("#btn-agregar");
     btnAgregar.addEventListener("click", () => { agregarElemento(obtenerObjeto()) });
     const btnAgregarCategoria = document.querySelector("#btn-agregar-categoria");
     btnAgregarCategoria.addEventListener("click", () => { agregarCategoria(obtenerObjeto(objetosServicios).categoria) });
-    // setTimeout(() =>{
-    //     let btnEliminar = document.querySelectorAll(".btn-eliminar");
-    //     btnEliminar.forEach(btn => { btn.addEventListener("click",eliminarElemento);});
-    //     document.querySelectorAll(".btn-eliminar").forEach(btn => { btn.addEventListener("click",eliminarElemento);});
-    // },1000);
+    const btnFilter = document.querySelectorAll(".tag");
+    btnFilter.forEach((btn) => btn.addEventListener("click", filter));
+    // const btnPagination = document.querySelectorAll(".btn-pagination");
+    // btnPagination.forEach((item) => item.addEventListener("click",actualizarPage));
 }
